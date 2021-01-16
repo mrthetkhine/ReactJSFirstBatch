9c42daed53a52962de56cc3c1b106f32f376ca61
Thet Khine <mrthetkhine@gmail.com>
2021-01-16 16:16:22 +0630

    Child Process

--- chapter16/ChildProcessDemo.js
+++ chapter16/ChildProcessDemo.js
@@ -1,6 +1,12 @@
 const child_process = require('child_process');
+const util = require('util');
 
 let listing  = child_process.execSync("ls -l",{
     encoding: "utf-8"
 });
 console.log('Listing ',listing);
+
+const exceP = util.promisify(child_process.exec);
+exceP('ls -l',{
+    encoding: "utf-8"
+}).then(result=>console.log('Result ',result));
