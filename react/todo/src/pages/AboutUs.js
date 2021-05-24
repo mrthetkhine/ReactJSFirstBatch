3a68a68dc30de4e03e2b09b1eafc9d263a840f81
Thet Khine <mrthetkhine@gmail.com>
2021-05-24 19:56:22 +0630

    Add Router

--- react/todo/src/pages/AboutUs.js
+++ react/todo/src/pages/AboutUs.js
@@ -1,15 +1,18 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Link, Switch, Redirect } from "react-router-dom";
+import { BrowserRouter as Router, Route, Link, useLocation, Redirect,useHistory } from "react-router-dom";
 
 export default function AboutUs(props)
 {
     console.log("Props ",props.match.params.id);
     let id = props.match.params.id;
+    let history = useHistory();
+    let {pathname} = useLocation();
     return(
         <div>
             { id == 404 ? <Redirect to="/" /> : null }
             <h1>About us id {id} Page</h1>
-            <button className="btn btn-primary" onClick={() => props.history.push('/') }>Go to Main</button>
+            <button className="btn btn-primary" onClick={() => history.push('/') }>Go to Main</button>
+            <h3>Path {pathname}</h3>
         </div>
     );
 
